8b38e4767610344f384887569c4bc20630822de3
Aldo Cortesi <aldo@nullcube.com>
2010-01-27 11:29:54 +1300

    Radix sort, contributed by Chris Wong (chrisyco@gmail.com)

--- libsortvis/sortable.py
+++ libsortvis/sortable.py
@@ -230,5 +230,40 @@ class Quick(Algorithm):
             if l < right:
                 self.sort(lst, l, right)
 
+class Radix(Algorithm):
+    """
+    A version of radix sort by Chris Wong.
+    <chrisyco@gmail.com>
+    """
+    name = "radixsort"
+    def sort(self, lst):
+        from itertools import chain
+        is_sorted = lambda l: all([a < b for a, b in zip(l[:-1], l[1:])])
+        shift = 1
+        zeroes = []
+        ones = []
+        while not is_sorted(lst.lst):
+            orig = lst.lst[:]
+            while len(orig) != 0:
+                # take an item out of the list
+                item = orig.pop(0)
+                # put it in the right bucket
+                if (item.i & shift) == 0:
+                    zeroes.append(item)
+                else:
+                    ones.append(item)
+                # copy the items back into the main list
+                for j, item in enumerate(chain(zeroes, orig, ones)):
+                    lst[j] = item
+                # for a more simple graph, comment out the line below
+                lst.log()
+                #
+                if is_sorted(lst):
+                    return
+            lst.log()
+            shift = shift << 1
+            zeroes[:] = []
+            ones[:] = []
 
-algorithms = [Tim, Quick, Heap, Selection, ListInsertion, Bubble, Shell]
+
+algorithms = [Tim, Quick, Heap, Selection, ListInsertion, Bubble, Shell, Radix]
