400b159e69226f64e79b11fb3f58260405e3c9af
Aldo Cortesi <aldo@nullcube.com>
2010-07-13 21:33:22 +1200

    Add smoothsort, contributed by Kevin Granade (kevin.granade@gmail.com)

--- libsortvis/sortable.py
+++ libsortvis/sortable.py
@@ -369,7 +369,118 @@ class Merge(Algorithm):
             i, end_i, j = i + 1, end_i + 1, j + 1
 
 
+class Smooth(Algorithm):
+    """
+    Algorithm from http://en.wikipedia.org/wiki/Smoothsort
+    """
+    name = "smoothsort"
+    # Possibly replace with a generator that produces Leonardo numbers?
+    # That would be of limited utility since this is all of them up to 31 bits.
+    LP = [ 1, 1, 3, 5, 9, 15, 25, 41, 67, 109, 177, 287, 465, 753, 1219, 1973,
+           3193, 5167, 8361, 13529, 21891, 35421, 57313, 92735, 150049, 242785,
+           392835, 635621, 1028457, 1664079, 2692537, 4356617, 7049155,
+           11405773, 18454929, 29860703, 48315633, 78176337, 126491971,
+           204668309, 331160281, 535828591, 866988873 ]
+
+    # Solution for determining number of trailing zeroes of a number's binary representation.
+    # Taken from http://www.0xe3.com/text/ntz/ComputingTrailingZerosHOWTO.html
+    # I don't much like the magic numbers, but they really are magic.
+    MultiplyDeBruijnBitPosition = [ 0,  1, 28,  2, 29, 14, 24, 3,
+                                    30, 22, 20, 15, 25, 17,  4, 8,
+                                    31, 27, 13, 23, 21, 19, 16, 7,
+                                    26, 12, 18,  6, 11,  5, 10, 9]
+    def trailingzeroes(self, v):
+        return self.MultiplyDeBruijnBitPosition[(((v & -v) * 0x077CB531L) >> 27) & 0b11111]
+
+    def sift(self, lst, pshift, head):
+        val = lst[head]
+        while pshift > 1:
+            rt = head - 1
+            lf = head - 1 - self.LP[pshift - 2]
+
+            if val >= lst[lf] and val >= lst[rt]:
+                break
+            if lst[lf] >= lst[rt]:
+                lst[head] = lst[lf]
+                head = lf
+                pshift -= 1
+                lst.log()
+            else:
+                lst[head] = lst[rt]
+                head = rt
+                pshift -= 2
+                lst.log()
+
+        if lst[head] != val:
+            lst[head] = val
+            lst.log()
+
+    def trinkle(self, lst, p, pshift, head, trusty):
+        val = lst[head]
+        while p != 1:
+            stepson = head - self.LP[pshift]
+            if lst[stepson] <= val:
+                break
+            if not trusty and pshift > 1:
+                rt = head - 1
+                lf = head - 1 - self.LP[pshift - 2]
+                if lst[rt] >= lst[stepson] or lst[lf] >= lst[stepson]:
+                    break
+
+            lst[head] = lst[stepson]
+            lst.log()
+
+            head = stepson
+            trail = self.trailingzeroes(p & ~1)
+            p >>= trail
+            pshift += trail
+            trusty = False
+
+        if not trusty:
+            lst[head] = val
+            lst.log()
+            self.sift(lst, pshift, head)
+
+    def sort(self, lst):
+        p = 1
+        pshift = 1
+        head = 0
+        while head < len(lst) - 1:
+            if (p & 3) == 3:
+                self.sift(lst, pshift, head)
+                p >>= 2
+                pshift += 2
+            else:
+                if self.LP[pshift - 1] >= len(lst) - 1 - head:
+                    self.trinkle(lst, p, pshift, head, False)
+                else:
+                    self.sift(lst, pshift, head)
+
+                if pshift == 1:
+                    p <<= 1
+                    pshift -= 1
+                else:
+                    p <<= pshift - 1
+                    pshift = 1
+            p |= 1
+            head += 1
+        self.trinkle(lst, p, pshift, head, False)
+        while pshift != 1 or p != 1:
+            if pshift <= 1:
+                trail = self.trailingzeroes(p & ~1)
+                p >>= trail
+                pshift += trail
+            else:
+                p <<= 2
+                p ^= 7
+                pshift -= 2
+
+                self.trinkle(lst, p >> 1, pshift + 1, head - self.LP[pshift] - 1, True)
+                self.trinkle(lst, p, pshift, head - 1, True)
+            head -= 1
+
+
 algorithms = [
     Quick, Tim, Heap, Selection, ListInsertion, Bubble, Shell, Radix, Stooge, 
-    Gnome, Cocktail, OddEven, Comb, Merge
+    Gnome, Cocktail, OddEven, Comb, Merge, Smooth
 ]
