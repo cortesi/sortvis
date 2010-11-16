4a00f6253ec26652931a31d067232669f34081d2
Joern Hees <gitdev@joernhees.de>
2010-11-17 00:11:09 +0100

    Comments on quicksort, which does unnecessary comparisons of elements with the same index (e.g., if lst[i] <= lst[i])

--- libsortvis/algos/quicksort.py
+++ libsortvis/algos/quicksort.py
@@ -1,11 +1,12 @@
+# note: quicksort does unnecessary comparisons of elements  with the same index 
 def quicksort(lst, left=0, right=None):
     if right is None:
         right = len(lst) - 1
     l = left
     r = right
-    if l <= r:
+    if l <= r: # why not < ?
         mid = lst[(left+right)/2]
-        while l <= r:
+        while l <= r: # why not < ?
             while l <= right and lst[l] < mid:
                 l += 1
             while r > left and lst[r] > mid:
